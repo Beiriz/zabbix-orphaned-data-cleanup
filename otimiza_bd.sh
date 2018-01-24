92f72354edb3a31f44136eac7d4c61683c169869
José Beiriz <josebeiriz@gmail.com>
2018-01-24 15:29:41 -0200

    Update and rename otimiza_bd.sql to otimiza_bd.sh

---
+++ otimiza_bd.sh
@@ -0,0 +1,2 @@
+# https://stackoverflow.com/questions/5474662/mysql-optimize-all-tables
+mysqlcheck -uroot -p -o zabbix
--- otimiza_bd.sql
+++
@@ -1,3 +0,0 @@
--- mysql zabbix -p ./otimiza_bd.sql
--- https://stackoverflow.com/questions/5474662/mysql-optimize-all-tables
-mysqlcheck -o zabbix
